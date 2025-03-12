a8561a3cfa96e176a6e8a7b13dfc23862f70ebc5
borishsanasam <borishsanasam93@gmail.com>
2025-03-12 14:40:34 +0530

    add1

---
+++ method/add.java
@@ -0,0 +1,17 @@
+package command;
+
+public class Bois {
+    /* 
+     * This program demonstrates the addition of two integer variables.
+     */
+    public static void main(String[] args) {
+        int firstNumber = 10;  // First integer value
+        int secondNumber = 20; // Second integer value
+        
+        // Adding the two numbers
+        int sum = firstNumber + secondNumber;
+        
+        // Printing the result
+        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
+    }
+}
