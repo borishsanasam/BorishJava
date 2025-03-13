91dc1362d98aadc7733fc159753ca2d0ba7551be
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 15:28:44 +0530

    add

---
+++ dynamicAddition/add.java
@@ -0,0 +1,14 @@
+package dynamicAddition;
+import java.util.*;
+public class add {
+    public static void main(String[] args) {
+        Scanner o=new Scanner(System.in);
+        System.out.println("enter any no.: " );
+        int no= o.nextInt();
+        Scanner w=new Scanner(System.in);
+        System.out.println("enter any no.: " );
+        int bn= w.nextInt();
+        int z=no+bn;
+        System.out.println("add: "+z);
+    }
+}
