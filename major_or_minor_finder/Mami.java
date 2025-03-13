2873089b92d7eb279185c6879bce4489a67a585f
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 11:37:30 +0530

    bro

---
+++ major_or_minor_finder/Mami.java
@@ -0,0 +1,11 @@
+public class Mami{
+    public static void main(String args[]){
+        int a=20;
+        if(a<=20){
+            System.out.println("minor");
+        }
+        else{
+            System.out.println("major");
+        }
+    }
+}
