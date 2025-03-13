887910bf2f6b5f094838e508872cd8cd784e6cd7
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 13:12:50 +0530

    ry

---
+++ Array/rray.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String args[]) {
+        int a[] = {10, 20, 30}; // Corrected array declaration
+        System.out.println(a[0]); // Print the first element
+        System.out.println(a[1]); // Print the second element
+        System.out.println(a[2]); // Print the third element
+    }
+}
