0f76fbfd83bb84d48058456746b26d4cfbcbbbfb
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 11:54:50 +0530

    negat

---
+++ negative/nega.java
@@ -0,0 +1,10 @@
+public class Nega {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a > 5) {
+            System.out.println("negative");
+        } else {
+            System.out.println("positive");
+        }
+    }
+}
