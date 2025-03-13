134a796338affca6bd32ed8b1bab926fd73027f3
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 13:31:20 +0530

    ball

---
+++ ball_colour/Ball.java
@@ -0,0 +1,9 @@
+package ball_colour;
+
+public class Ball {
+    public static void main(String args[]) {
+        String a[] = {"Blue", "White", "Black"};
+        
+        System.out.println(a[2]); // This will print "Black"
+    }
+}
