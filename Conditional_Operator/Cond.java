bbe4ec3c54850534a63c88ea0a4b5e5a6abb8c20
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 10:38:54 +0530

    condition

---
+++ Conditional_Operator/Cond.java
@@ -0,0 +1,10 @@
+package Conditional_Operator;
+public class Cond{
+    public static void main(String args[]){
+        System.out.println(10<2);
+         System.out.println(10>2);
+          System.out.println(10<=2);
+           System.out.println(10==2);
+            System.out.println(10!=2);
+    }
+}
