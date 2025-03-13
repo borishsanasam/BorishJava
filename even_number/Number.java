e6c6d1f93fc4ad4249fbb39c72e15f911d0cf34a
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 14:20:34 +0530

    even

---
+++ even_number/Number.java
@@ -0,0 +1,10 @@
+public class Number {
+    public static void main(String args[]) {
+        System.out.println("Even numbers between 11 and 20:");
+        for (int i = 11; i < 20; i++) {
+            if (i % 2 == 0) { 
+                System.out.println(i); 
+            }
+        }
+    }
+}
