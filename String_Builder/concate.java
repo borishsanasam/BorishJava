05f95cf3fd62f221eeb4277860389bb47611b68b
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 11:57:48 +0530

    conc

---
+++ String_Builder/concate.java
@@ -0,0 +1,11 @@
+public class Concate{
+    void concate(String a1,String a2){
+        String a3 = a1 + a2;
+        System.out.println(a3);
+    }
+    public static void main(String args[]){
+        Concate obj = new Concate();
+        obj.concate("Samir","Dolly");
+
+    }
+}
