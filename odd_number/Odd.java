bd43e364c8656361059bec38d3a9ff78bad7e0e2
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 14:26:41 +0530

    odd

---
+++ odd_number/Odd.java
@@ -0,0 +1,10 @@
+public class Number {
+    public static void main(String args[]) {
+         for (int i = 11; i < 20; i++) {
+        System.out.println("Odd numbers between 11 and 20:");
+            if (i % 2 != 0) { 
+                System.out.println(i); 
+            }
+        }
+    }
+}
