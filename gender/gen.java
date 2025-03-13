3cb85996846048a8544aa269c721653f115c48e5
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 12:11:27 +0530

    gend

---
+++ gender/gen.java
@@ -0,0 +1,12 @@
+package gender;
+public class gen{
+    public static void main(String args[]){
+        String a= "m";
+        if(a=="m"){
+            System.out.println("male");
+        }
+        else{
+            System.out.println("female");
+        }
+    }
+}
