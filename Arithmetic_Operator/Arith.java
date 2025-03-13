584f1c6d97c3e6548758261146cd160ce008fde9
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 10:29:29 +0530

    oper

---
+++ Arithmetic_Operator/Arith.java
@@ -0,0 +1,9 @@
+public class Arith{
+    public static void main(String args[]){
+        System.out.println(10+2);
+         System.out.println(10-2);
+          System.out.println(10*2);
+           System.out.println(10/2);
+            System.out.println(10%2);
+    }
+}
