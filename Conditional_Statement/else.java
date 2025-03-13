f2b03154595565d6d940ef27b15f573d0541e795
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 13:21:17 +0530

    eme

---
+++ Conditional_Statement/else.java
@@ -0,0 +1,11 @@
+public class Else{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("poirei is good");
+        }
+        else{
+            System.out.println("who is he");
+             
+        }
+    }
+}
