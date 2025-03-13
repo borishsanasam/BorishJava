88d5ed53e6a59d2f28a89132409cfec85d71c2c4
borishsanasam <borishsanasam93@gmail.com>
2025-03-13 11:04:52 +0530

    state

---
+++ Conditional_Statement/State.java
@@ -0,0 +1,11 @@
+public class State{
+    public static void main(String args[]){
+        int a=20;
+        if(a>18){
+         System.out.println("True");
+        }
+        if(a<18){
+         System.out.println("false");
+        }
+    }
+}
