de101a61b04305f77fc84ff9ea386d0998cf9846
borishsanasam <borishsanasam93@gmail.com>
2025-03-14 12:11:40 +0530

    index

---
+++ index/ind.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ind{
+    public static void main(String args[]){
+        String name = "hello,world";
+        int index = name.indexOf("w");
+       
+          System.out.println(name.indexOf("w"));
+    }
+}
