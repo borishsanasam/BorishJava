732c3a7aa9ae2e8f5a90f97d8f9e97a8d6fedfb2
borishsanasam <borishsanasam93@gmail.com>
2025-03-14 10:28:37 +0530

    number

---
+++ finding_n_num/Findn.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+public class Findn{
+    public static void main(String args[]){
+        Scanner obj =new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int a = obj.nextInt();
+    int i =1;
+    while(i<=a){
+        System.out.println(i);
+        i++;
+    }
+    }
+}
