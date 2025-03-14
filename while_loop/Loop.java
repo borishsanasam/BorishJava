19dbe3eddda00cd743da429a9daa2381f04077e0
borishsanasam <borishsanasam93@gmail.com>
2025-03-14 10:02:58 +0530

    lop

---
+++ while_loop/Loop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Loop{
+    public static void main(String args[]){
+        int i = 0;
+        while(i<10){
+        System.out.println(i);
+        i=i+1;
+        }
+        }
+    }
