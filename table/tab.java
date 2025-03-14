e0c4f967eb758c1c44e39d995061f4a069c3afee
borishsanasam <borishsanasam93@gmail.com>
2025-03-14 10:14:58 +0530

    tabl

---
+++ table/tab.java
@@ -0,0 +1,12 @@
+public class tab{
+    public static void main(String args[]){
+        int i = 1;
+        int j = 7;
+        while(i<=10){
+            int a = i * 7;
+            System.out.println("7*"+i +"=" +a);
+            i=i+1;
+        }
+
+    }
+}
