a86651c96474226ac014aaf21111850fc0d535a2
borishsanasam <borishsanasam93@gmail.com>
2025-03-15 11:12:44 +0530

    sum

---
+++ sum_of_digit/Dig.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Dig {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);      
+                System.out.println("Enter the number:");
+        int i = scanner.nextInt();          
+              System.out.println("Enter the number:");
+        int j = scanner.nextInt();          
+               if (i > j) {
+            System.out.println("number.");
+        } else {
+            int sum = 0;
+            
+            
+            for (int k = i; k <= j; k++) {
+                sum += k; 
+            }
+            
+           
+            System.out.println("The total sum " + i + " to " + j + " is: " + sum);
+        }
+        
+        scanner.close(); 
+            }
+}
